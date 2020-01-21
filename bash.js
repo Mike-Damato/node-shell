7c4af42c472c9e3831b458cccddf058ae6785dc2
tianxinAngland <ts682@cornell.edu>
2020-01-21 12:22:15 -0500

    implemented ls

--- bash.js
+++ bash.js
@@ -1,3 +1,6 @@
+const pwd = require('./pwd');
+const ls = require('./ls');
+
 //Output a prompt
 process.stdout.write('prompt > ');
 
@@ -9,10 +12,11 @@ process.stdin.on('data', (data) => {
   const cmd = data.toString().trim(); //remove the new line
 
   if(cmd === 'pwd'){
-    console.log(`Current directory: ${process.cwd()}`);
+    pwd();
+  }else if(cmd === 'ls'){
+    ls();
   }else{
     process.stdout.write('You typed: ' + cmd);
   }
-
     process.stdout.write('\nprompt > ');
 });
